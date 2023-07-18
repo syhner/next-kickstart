fb0c69b7466cc1c2fb53891d99aa541c6285387b
Siraj Hussain <71605633+Syhner@users.noreply.github.com>
2023-07-18 20:19:02 +0200

    use edge runtime on homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import Image from 'next/image';
 
+export const runtime = 'edge';
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
