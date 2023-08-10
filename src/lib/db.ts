2770401bef091c9991f84f46d36d82589d061036
Siraj Hussain <71605633+Syhner@users.noreply.github.com>
2023-08-10 23:25:39 +0200

    support relational orm queries

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,9 +1,10 @@
-import { connect } from '@planetscale/database';
-import { drizzle } from 'drizzle-orm/planetscale-serverless';
-import { env } from '~/env.mjs';
+// import { connect } from '@planetscale/database';
+// import { drizzle } from 'drizzle-orm/planetscale-serverless';
+// import * as schemas from '~/db/schemas';
+// import { env } from '~/env.mjs';
 
 // const connection = connect({
 //   url: env.DATABASE_URL,
 // });
 
-// export const db = drizzle(connection);
+// export const db = drizzle(connection, { schema: schemas });
