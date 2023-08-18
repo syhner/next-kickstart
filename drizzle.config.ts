a5aee74979baa93aded9e7c9714f97f7114c8918
Siraj Hussain <71605633+Syhner@users.noreply.github.com>
2023-08-19 00:41:26 +0200

    use parsed env for db connection with drizzle

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,16 +1,11 @@
-/* eslint-disable no-process-env */
+// import type { Config } from 'drizzle-kit';
+// import { env } from './src/env.mjs';
 
-import * as dotenv from 'dotenv';
-import type { Config } from 'drizzle-kit';
-import { z } from 'zod';
-
-dotenv.config({ path: '.env.local' });
-
-export default {
-  schema: './src/db/schemas/*.ts',
-  out: './src/db/migrations',
-  driver: 'mysql2',
-  dbCredentials: {
-    connectionString: z.string().parse(process.env.DATABASE_URL),
-  },
-} satisfies Config;
+// export default {
+//   schema: './src/db/schemas/*.ts',
+//   out: './src/db/migrations',
+//   driver: 'mysql2',
+//   dbCredentials: {
+//     connectionString: env.DATABASE_URL,
+//   },
+// } satisfies Config;
