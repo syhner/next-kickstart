e9ddb1f0f2c974f44b00e39bfbb2d43dfb5543e1
Siraj Hussain <71605633+Syhner@users.noreply.github.com>
2023-08-23 06:49:02 +0200

    remove health route handler

--- src/app/health/route.ts
+++
@@ -1,8 +0,0 @@
-import { ServerRuntime } from 'next';
-import { NextResponse } from 'next/server';
-
-export const runtime: ServerRuntime = 'edge';
-
-export async function GET(request: Request) {
-  return NextResponse.json({ status: 'ok' });
-}
