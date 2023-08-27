d46731457453ebee1579c5a467d0bdd07494438e
Siraj Hussain <71605633+Syhner@users.noreply.github.com>
2023-08-27 15:36:23 +0200

    use React Hook Form in websockets example

--- src/app/examples/websockets/page.tsx
+++ src/app/examples/websockets/page.tsx
@@ -1,15 +1,36 @@
 'use client';
 
 import { useState } from 'react';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
 import { Button } from '~/components/ui/button';
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '~/components/ui/form';
 import { Input } from '~/components/ui/input';
 import { useEvent } from '~/hooks/useEvent';
 import { trpcReact } from '~/trpc/trpc-react';
 
-export default function Page() {
-  const [inputMessage, setInputMessage] = useState('');
-  const [messages, setMessages] = useState<string[]>([]);
+const formSchema = z.object({
+  message: z
+    .string()
+    .min(2, {
+      message: 'Message must be at least 2 characters',
+    })
+    .max(50, {
+      message: 'Message must be not exceed 50 characters',
+    }),
+});
 
+export default function Page() {
+  const [messages, setMessages] = useState<string[]>([]);
   const addMessage = trpcReact.message.useMutation();
 
   useEvent({
@@ -20,27 +41,40 @@ export default function Page() {
     },
   });
 
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      message: '',
+    },
+  });
+
+  const onSubmit = (values: z.infer<typeof formSchema>) => {
+    addMessage.mutate({ message: values.message });
+  };
+
   return (
     <div className='mx-24 mt-12'>
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          addMessage.mutate({ message: inputMessage });
-          setInputMessage('');
-        }}
-      >
-        <div className='flex w-full max-w-sm items-center space-x-2'>
-          <Input
-            value={inputMessage}
-            onChange={(e) => setInputMessage(e.target.value)}
-            placeholder='Your message...'
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className='inline'>
+          <FormField
+            control={form.control}
+            name='message'
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <Input
+                    className='max-w-sm'
+                    placeholder='Your message...'
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
           />
-          <Button type='submit' disabled={inputMessage.length === 0}>
-            Send
-          </Button>
-        </div>
-      </form>
-
+          <Button type='submit'>Send</Button>
+        </form>
+      </Form>
       <div className='mt-6'>
         {messages.map((message, idx) => (
           <p key={idx}>{message}</p>
