7a993a02897c79795b11cbecce097fa485d94d0f
Siraj Hussain <71605633+Syhner@users.noreply.github.com>
2023-09-06 03:34:35 +0200

    fix trpc router test by mocking next-auth

--- testing/setup.ts
+++ testing/setup.ts
@@ -1,8 +1,18 @@
 import matchers from '@testing-library/jest-dom/matchers';
 import { cleanup } from '@testing-library/react';
-import { afterEach, expect } from 'vitest';
+import { afterEach, expect, vi } from 'vitest';
 
 expect.extend(matchers);
+
+vi.mock('next-auth', () => {
+  const auth = () => ({ user: null });
+  return {
+    default: () => ({
+      auth,
+      handlers: {},
+    }),
+  };
+});
 
 // Hooks are reset before each suite
 afterEach(() => {
