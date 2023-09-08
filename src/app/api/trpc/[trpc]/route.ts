2df4a8b88b6be4333912089bd36dd80dee187f4a
Siraj Hussain <71605633+Syhner@users.noreply.github.com>
2023-09-09 01:35:48 +0200

    remove unused server runtime
    edge is already set from the root layout

--- src/app/api/trpc/[trpc]/route.ts
+++ src/app/api/trpc/[trpc]/route.ts
@@ -1,9 +1,6 @@
-import { ServerRuntime } from 'next';
 import { fetchRequestHandler } from '@trpc/server/adapters/fetch';
 import { createContext } from '~/trpc/init';
 import { appRouter } from '~/trpc/router';
-
-export const runtime: ServerRuntime = 'edge';
 
 const handler = (req: Request) =>
   fetchRequestHandler({
