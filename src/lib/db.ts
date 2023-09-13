a2a2f5da898a47d558338d26dd659af3a317e766
Siraj Hussain <71605633+Syhner@users.noreply.github.com>
2023-09-14 01:30:50 +0200

    remove barrel file for schemas
    for maintainability

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -3,11 +3,11 @@
  */
 // import { connect } from '@planetscale/database';
 // import { drizzle } from 'drizzle-orm/planetscale-serverless';
-// import * as schemas from '~/db/schemas';
+// import * as authSchemas from '~/db/schemas/auth';
 // import { env } from '~/env.mjs';
 //
 // const connection = connect({
 //   url: env.DATABASE_URL,
 // });
 //
-// export const db = drizzle(connection, { schema: schemas });
+// export const db = drizzle(connection, { schema: {...authSchemas} });
