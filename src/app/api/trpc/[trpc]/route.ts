8cb404bb13628d66fc4bf05b620bcc1ea8e750de
Siraj Hussain <71605633+Syhner@users.noreply.github.com>
2023-11-29 20:02:53 +0100

    normalize request param of route handlers

--- src/app/api/trpc/[trpc]/route.ts
+++ src/app/api/trpc/[trpc]/route.ts
@@ -1,11 +1,12 @@
+import { NextRequest } from 'next/server';
 import { fetchRequestHandler } from '@trpc/server/adapters/fetch';
 import { createContext } from '~/trpc/init';
 import { appRouter } from '~/trpc/router';
 
-const handler = (req: Request) =>
+const handler = (request: NextRequest) =>
   fetchRequestHandler({
     endpoint: '/api/trpc',
-    req,
+    req: request,
     router: appRouter,
     createContext,
   });
